ace1b8974e7fbe18ab22a30c42c1fc9d239c248f
FranCambronero90 <78375831+FranCambronero90@users.noreply.github.com>
2021-02-22 20:20:21 -0600

    Update Busqueda.java

--- BN_Subastas/src/main/java/com/ucreativa/Busqueda.java
+++ BN_Subastas/src/main/java/com/ucreativa/Busqueda.java
@@ -3,42 +3,50 @@
  */
 package com.ucreativa;
 
+
 /**
  * 
  *
  */
-public class Busqueda implements Bien{
-	private String listaBienes;
-	
-	/**
-	 * @return the listaBienes
-	 */
-	public String getListaBienes() {
-		return listaBienes;
-	}
+public class Busqueda extends Bien{
+    private String listaBienes;
+    
+    /**
+     * @return the listaBienes
+     */
+    public String getListaBienes() {
+        return listaBienes;
+    }
+
+ 
 
 
-	/**
-	 * @param listaBienes the listaBienes to set
-	 */
-	public void setListaBienes(String listaBienes) {
-		this.listaBienes = listaBienes;
-	}
+    /**
+     * @param listaBienes the listaBienes to set
+     */
+    public void setListaBienes(String listaBienes) {
+        this.listaBienes = listaBienes;
+    }
 
-	/**
-	 * constructor
-	 * @param listaBienes
-	 */
-	public Busqueda(String listaBienes) {
-		super();
-		this.listaBienes = listaBienes;
-	}
+ 
 
 
-	@Override
-	public String toString() {
-		return "Busqueda [listaBienes=" + listaBienes + "]";
-	}
-	
+    @Override
+    public String toString() {
+        return "Busqueda [listaBienes=" + listaBienes + "]";
+    }
 
+ 
+
+    /**
+     * constructor
+     * @param id
+     * @param precio
+     * @param listaBienes
+     */
+    public Busqueda(String id, double precio, String listaBienes) {
+        super(id, precio);
+        this.listaBienes = listaBienes;
+    }
+    
 }
